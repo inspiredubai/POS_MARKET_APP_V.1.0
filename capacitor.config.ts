1bef96681e6b87fa81b59907a555b32592ed27a5
AFSAR RIAZ <AFSAR RIAZ@DESKTOP-QP7DBD2>
2025-08-13 11:41:09 +0500

    capasitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'POS_Market',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    androidScheme: 'http',
+    cleartext: true,
+    allowNavigation: [
+      'http://103.74.54.207:8207/api/*'
+    ]
+  }
 };
 
 export default config;
