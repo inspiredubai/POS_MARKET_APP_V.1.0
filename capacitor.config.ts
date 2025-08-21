9dbbda852b20277decef40c9638f814fca05fa17
AFSAR RIAZ <AFSAR RIAZ@DESKTOP-QP7DBD2>
2025-08-21 11:07:33 +0500

    a

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -8,7 +8,7 @@ const config: CapacitorConfig = {
     androidScheme: 'http',
     cleartext: true,
     allowNavigation: [
-      'http://103.74.54.207:8207/api/*',
+      'http://103.74.54.207:8064/api/*',
       'http://103.74.54.207:8585/api/*'
     ]
   }
