0580fa630d5b14e25624230216ae7816656fe675
hussnag <husnag768@gmail.com>
2025-08-21 15:33:29 +0500

    12

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,10 @@
 export const environment = {
   production: true,
-  // apiRootURL: 'http://103.74.54.207:8207/api/',
+  apiRootURL: 'http://103.74.54.207:8207/api/',
   // apiRootURL:'http://localhost:53447/api/',
-  apiRootURL: 'http://103.74.54.207:8064/api/*',
+  // apiRootURL: 'http://103.74.54.207:8064/api/',
   reportApiUrl: 'http://103.74.54.207:8585/api/',
-  cutomerApiBaseUrl: 'http://103.74.54.207:8064/api/'
+  // cutomerApiBaseUrl: 'http://103.74.54.207:8064/api/'
 
 };
+  
