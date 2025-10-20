6ca8b2d7e44cc6fa301595ab48618fd01f8cdbb5
AFSAR RIAZ <AFSAR RIAZ@DESKTOP-QP7DBD2>
2025-10-20 18:54:36 +0500

    update

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,7 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { LoginService } from 'src/app/service/login.service';
@@ -11,59 +16,66 @@ import { ToastService } from 'src/app/service/toast.service';
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-    standalone: true,
-    imports: [CommonModule, IonicModule, FormsModule, HttpClientModule, ReactiveFormsModule],
-    providers: [LoginService],
+  standalone: true,
+  imports: [
+    CommonModule,
+    IonicModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
+  providers: [LoginService],
 })
-export class LoginComponent  implements OnInit {
-loading:boolean=false
-salesForm:any
-  constructor(private router: Router,
-              private loginApi:LoginService,
-              private toastService:ToastService,
-              private fb: FormBuilder
-  ) { }
-  showSales(){
-    this.router.navigate(['/home'])
+export class LoginComponent implements OnInit {
+  loading: boolean = false;
+  salesForm: any;
+  constructor(
+    private router: Router,
+    private loginApi: LoginService,
+    private toastService: ToastService,
+    private fb: FormBuilder
+  ) {}
+  showSales() {
+    this.router.navigate(['/home']);
   }
   ngOnInit() {
-     this.salesForm = this.fb.group({
-         username : ['', Validators.required],
-          password: ['', Validators.required],
-     })
+    this.salesForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
   }
-loginRequest() {
-  this.loading=true
-  const payload = {
-    username:  this.salesForm.value.username,
-    password: this.salesForm.value.password,
-    rememberme: false,
-    financialperiod: {
-      financialPeriodsFsno: 1,
-      financialPeriodsStartDate: "2023-01-01T00:00:00",
-      financialPeriodsEndDate: "2023-12-31T00:00:00"
-    },
-    location: {
-      locationMasterLocationId: 10,
-      locationMasterLocationName: "Main-10",
-      locationMasterLocationStatus: null
-    }
-  };
+  loginRequest() {
+    this.loading = true;
+    const payload = {
+      username: this.salesForm.value.username,
+      password: this.salesForm.value.password,
+      rememberme: false,
+      financialperiod: {
+        financialPeriodsFsno: 1,
+        financialPeriodsStartDate: '2023-01-01T00:00:00',
+        financialPeriodsEndDate: '2023-12-31T00:00:00',
+      },
+      location: {
+        locationMasterLocationId: 10,
+        locationMasterLocationName: 'Main-10',
+        locationMasterLocationStatus: null,
+      },
+    };
 
-  this.loginApi.loginRequest(payload).subscribe({
-    next: (res: any) => {
-      if (res?.valid) {
-        this.router.navigate(['/dashboard/']);
-          this.loading=false
-      } else {
-        this.toastService.show('Invalid Credential', 'danger');
-          this.loading=false
-      }
-    },
-    error: (err) => {
+    this.loginApi.loginRequest(payload).subscribe({
+      next: (res: any) => {
+        if (res?.valid) {
+          this.router.navigate(['/dashboard/']);
+          this.loading = false;
+        } else {
+          this.toastService.show('Invalid Credential', 'danger');
+          this.loading = false;
+        }
+      },
+      error: (err) => {
+        this.loading = false;
         this.toastService.show('Something Went wrong', 'danger');
-    }
-  });
-}
-
+      },
+    });
+  }
 }
