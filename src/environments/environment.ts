81d9e75443c7ce5381b4af0a14a3b624e4049898
AFSAR RIAZ <AFSAR RIAZ@DESKTOP-QP7DBD2>
2025-11-21 05:46:50 +0500

    b

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,7 @@ export const environment = {
   production: false,
   //  apiRootURL: 'http://103.74.54.207:8207/api/',
   //  apiRootURL:'http://localhost:53447/api/',
-
-  apiRootURL: 'http://103.74.54.207:8086/api/',
-
-  // apiRootURL: 'http://194.233.95.37:8064/api/',
+  apiRootURL: 'http://194.233.95.37:8064/api/',
   reportApiUrl: 'http://194.233.95.37:8085/api/',
 };
 
